8050ad6406499b88cbd92ff99c9bfc6942254217
이상규 <sam3970@gmail.com>
2020-06-23 01:30:34 +0900

    Fibonacci Dynamic Programming

--- [DP]Fibonacci.cpp
+++ [DP]Fibonacci.cpp
@@ -20,6 +20,23 @@ int fibonacci(int num)
 	/* Dynamic Programming apply part */
 	if (vc[num] != -1)
 	{
-		ret
+		return vc[num];
 	}
+
+	vc[num] = fibonacci(num - 1) + fibonacci(num - 2);
+	cout << num << "의 값" << vc[num] << endl;
+	return vc[num];
+}
+
+int main()
+{
+	int value;
+	cout << "피보나치 수열에 할당할 값 입력:";
+	cin >> value;
+
+	/* +1의 이유는 개행문자로 인함. 각각의 피보나치 수치를 -1로 초기화 */
+	vc.resize(value + 1, -1);
+
+	cout << "피보나치 수열 값의 합:" << fibonacci(value) << endl;
+	system("pause");
 }
