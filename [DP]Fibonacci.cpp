90c3e23ba123d99e266efdb9c011865b52aea064
이상규 <sam3970@gmail.com>
2020-06-23 00:46:41 +0900

    초기 커밋.

---
+++ [DP]Fibonacci.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> vc;
+
+int fibonacci(int num)
+{
+	if (num == 0)
+	{
+		return 0;
+	}
+	
+	if (num == 1)
+	{
+		return 1;
+	}
+	
+	/* Dynamic Programming apply part */
+	if (vc[num] != -1)
+	{
+		ret
+	}
+}
